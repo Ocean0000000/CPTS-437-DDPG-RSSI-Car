3f15bd66314fbeb86bc71c11dd4be157fd6aa7e3
Ocean0000000 <vantocean509@gmail.com>
2024-12-03 18:00:23 -0800

    Added trials 10-15 and documentation

--- src/observe.py
+++ src/observe.py
@@ -21,9 +21,6 @@ def nn_control(state: np.ndarray) -> float:
     return a
 
 # CREATE ENVIRONMENT
-# env = sim.Environment(dt=dt, x_bounds=[-2,2], y_bounds=[-2, 2], memory_size=memory_size, sensor_names=sensor_names,
-#                       obstacle_count=obstacle_count, obstacle_size=obstacle_size, seed=seed, render_type="human", nn_control=nn_control,
-#                       obstacle_types=obstacle_types, obstacle_proportions=obstacle_proportions)
 env = sim.Environment(dt=dt, x_bounds=x_bounds, y_bounds=y_bounds, memory_size=memory_size, sensor_names=sensor_names,
                       obstacle_count=obstacle_count, obstacle_size=obstacle_size, seed=seed, render_type="human", nn_control=nn_control,
                       obstacle_types=obstacle_types, obstacle_proportions=obstacle_proportions)
